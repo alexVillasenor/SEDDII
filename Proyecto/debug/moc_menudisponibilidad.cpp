91dcc816de1a181315d46cefe2b06c3663cd3b9b
Flyer <alejandro.villasenor97@gmail.com>
2018-11-02 04:14:35 -0600

    platilla modificar, respaldo

--- Proyecto/debug/moc_menudisponibilidad.cpp
+++ Proyecto/debug/moc_menudisponibilidad.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MenuDisponibilidad_t {
-    QByteArrayData data[11];
-    char stringdata0[207];
+    QByteArrayData data[12];
+    char stringdata0[231];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -41,7 +41,8 @@ QT_MOC_LITERAL(6, 101, 23), // "on_pushButton_3_clicked"
 QT_MOC_LITERAL(7, 125, 27), // "on_tabWidget_currentChanged"
 QT_MOC_LITERAL(8, 153, 5), // "index"
 QT_MOC_LITERAL(9, 159, 23), // "on_pushButton_4_clicked"
-QT_MOC_LITERAL(10, 183, 23) // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(10, 183, 23), // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(11, 207, 23) // "on_pushButton_6_clicked"
 
     },
     "MenuDisponibilidad\0on_pushButton_clicked\0"
@@ -49,7 +50,8 @@ QT_MOC_LITERAL(10, 183, 23) // "on_pushButton_5_clicked"
     "on_comboBox_Carrera_activated\0arg1\0"
     "on_pushButton_3_clicked\0"
     "on_tabWidget_currentChanged\0index\0"
-    "on_pushButton_4_clicked\0on_pushButton_5_clicked"
+    "on_pushButton_4_clicked\0on_pushButton_5_clicked\0"
+    "on_pushButton_6_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -59,7 +61,7 @@ static const uint qt_meta_data_MenuDisponibilidad[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       7,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -67,13 +69,14 @@ static const uint qt_meta_data_MenuDisponibilidad[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   49,    2, 0x08 /* Private */,
-       3,    0,   50,    2, 0x08 /* Private */,
-       4,    1,   51,    2, 0x08 /* Private */,
-       6,    0,   54,    2, 0x08 /* Private */,
-       7,    1,   55,    2, 0x08 /* Private */,
-       9,    0,   58,    2, 0x08 /* Private */,
-      10,    0,   59,    2, 0x08 /* Private */,
+       1,    0,   54,    2, 0x08 /* Private */,
+       3,    0,   55,    2, 0x08 /* Private */,
+       4,    1,   56,    2, 0x08 /* Private */,
+       6,    0,   59,    2, 0x08 /* Private */,
+       7,    1,   60,    2, 0x08 /* Private */,
+       9,    0,   63,    2, 0x08 /* Private */,
+      10,    0,   64,    2, 0x08 /* Private */,
+      11,    0,   65,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -81,6 +84,7 @@ static const uint qt_meta_data_MenuDisponibilidad[] = {
     QMetaType::Void, QMetaType::QString,    5,
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -100,6 +104,7 @@ void MenuDisponibilidad::qt_static_metacall(QObject *_o, QMetaObject::Call _c, i
         case 4: _t->on_tabWidget_currentChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 5: _t->on_pushButton_4_clicked(); break;
         case 6: _t->on_pushButton_5_clicked(); break;
+        case 7: _t->on_pushButton_6_clicked(); break;
         default: ;
         }
     }
@@ -130,13 +135,13 @@ int MenuDisponibilidad::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 7)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 7;
+        _id -= 8;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 7)
+        if (_id < 8)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 7;
+        _id -= 8;
     }
     return _id;
 }
